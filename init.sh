d55e2f1ca84ff59b1c5dfe4133cf78648d1ac55c
Tim Chung <tea24864@gmail.com>
2026-07-08 21:46:14 -0500

    Added nvim config

---
+++ init.sh
@@ -0,0 +1,5 @@
+# Run the following command to instgall Determinate on Linux and WSL
+# curl -fsSL https://install.determinate.systems/nix | sh -s -- install
+# Run the following commands to initialize after Determinate Nix is installed
+# . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+# nix run home-manager/master -- init --switch
